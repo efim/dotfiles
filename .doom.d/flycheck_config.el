73fd04dc2852112c8a741c71ab9e81beacff28a8
Nefedov <efim.nefedov@nordigy.ru>
2020-08-28 17:58:35 +0300

    moving .doom to headless repo for all dotfiles

---
+++ .doom.d/flycheck_config.el
@@ -0,0 +1,91 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+(after! counsel
+  (define-key!
+    [remap evil-show-marks]          #'counsel-evil-marks
+    )
+  )
+
+(setq-default evil-escape-key-sequence "kj") ;; from stack quection: https://stackoverflow.com/questions/10569165/how-to-map-jj-to-esc-in-emacs-evil-mode
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+(setq org-roam-directory "~/org/")
+(setq org-journal-dir "~/org/Journal/")
+(setq org-journal-file-format "%Y-%m-%d.org")
+(setq org-journal-date-format "%d.%m.%Y")
+
+(after! org
+  (setq org-capture-todo-file "gtd/inbox.org")
+  ;; agenda files are roots to org agenda search
+  (setq org-agenda-files '("~/org/gtd/inbox.org"
+                          "~/org/gtd/gtd.org"
+                          "~/org/gtd/tickler.org"
+        "~/org/Work"))
+
+  ;; GTD: setup for [[https://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html][gtd inspired]] refile and capture
+  (setq org-refile-targets '(("~/org/gtd/gtd.org" :maxlevel . 3)
+                            ("~/org/gtd/someday.org" :level . 1)
+                            ("~/org/gtd/tickler.org" :maxlevel . 2)
+                            ("~/org/writing-inbox.org" :maxlevel . 2)))
+
+  (setq org-capture-templates '(("t" "Todo [inbox]" entry
+                                (file+headline "~/org/gtd/inbox.org" "Tasks")
+                                "* TODO %i%?")
+                                ("T" "Tickler" entry
+                                (file+headline "~/org/gtd/tickler.org" "Tickler")
+                                "* %i%? \n %U")))
+)
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+
+(setq projectile-project-search-path '("~/Documents/"))
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+(require `epa-file)
+(epa-file-enable)

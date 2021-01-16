3440e18edaf70d5211fe39208ff7a32be914c670
efim <efim.nefedov@nordigy.ru>
2021-01-16 18:37:41 +0300

    emacs doom theme change committed

--- emacs-doom/config/config.el
+++ emacs-doom/config/config.el
@@ -22,7 +22,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;; (setq doom-theme 'doom-one)
+(setq doom-theme 'doom-one-light)
 
 (setq doom-font (font-spec :family "monospace" :size 16 :weight 'semi-light)
       doom-variable-pitch-font (font-spec :family "sans" :size 17))

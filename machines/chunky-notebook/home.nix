566c55f645196f17588ed034905130a8e6a46cbb
efim <efim.nefedov@nordigy.ru>
2021-10-31 12:14:03 +0300

    doom emacs reenabling mermaid
    it was disabled when I was moving to the (then stable nixpkgs) flakes
    (since I didn't want to also figure out how to add unstable nixpkgs)
    now I'm on unstable, so adding it is easy

--- machines/chunky-notebook/home.nix
+++ machines/chunky-notebook/home.nix
@@ -30,31 +30,6 @@
 
     my-screen-locker.isNixManaged = true;
 
-    # TODO ? use unstable from inputs of flake?
-    # home.packages = let
-    #   unstableTarball =
-    #     fetchTarball {
-    #       name = "nixos-unstable-2020-04-23";
-    #       # Commit hash for nixos-unstable at https://github.com/NixOS/nixpkgs/tree/nixos-unstable
-    #       url = https://github.com/NixOS/nixpkgs/archive/fd5c6aada3c38e7410f130ebf8fb01ef00257cd7.tar.gz;
-    #       # Hash obtained using `nix-prefetch-url --unpack <url>`
-    #       sha256 = "1aa55yca1z592ir4h499akyg2svg2fh730pxz8xkkcjdc9yq738m";
-    #     };
-    #   unstable = import unstableTarball {
-    #     config = config.nixpkgs.config;
-    #   };
-    #   in [
-    #     # unstable.nyxt # not really using this
-    #     # unstable.nodePackages.mermaid-cli
-    #     # TODO fails after home-manager flake module:
-    #     # error: attribute 'currentSystem' missing
-    #      # at /nix/store/9aq55g7bvwpxdg8wnvmdcn99ykhdnrz8-nixos-unstable-2020-04-23/pkgs/top-level/impure.nix:18:43:
-    #      #     17|   # (build, in GNU Autotools parlance) platform.
-    #      #     18|   localSystem ? { system = args.system or builtins.currentSystem; }
-    #      #       |                                           ^
-    #      #     19|
-    # ];
-
     my-autorandr = {
       # laptop display
       display1 = {

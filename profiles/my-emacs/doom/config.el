67541445b32242852e2866ed8fec903cc2aba45f
efim <efim.nefedov@nordigy.ru>
2021-11-05 11:08:02 +0300

    doom emacs trying out new theme

--- profiles/my-emacs/doom/config.el
+++ profiles/my-emacs/doom/config.el
@@ -26,7 +26,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;; (setq doom-theme 'doom-one)
-(setq doom-theme 'doom-one-light)
+(setq doom-theme 'doom-monokai-machine)
 
 (setq
  doom-font (font-spec :name "Iosevka" :size 20 )

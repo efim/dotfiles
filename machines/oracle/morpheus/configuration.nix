aa4409555061ce19fd38b7f4b87d406380286318
efim <efim.wool@gmail.com>
2022-01-17 21:38:40 +0300

    nix morpheus dns fix
    `cacheNetworks` is necessary to allow "recursive lookups", i.e
    forwarding requests upstream to resolve zones that aren't covered
    locally.
    
    "forwarders" are taken from "networking.nameservers", so would include 1.1.1.1
    
    now for all requests from tailscale (this is more wide, than I want
    actually) dns server would forward requests to parents, and be able to
    resolve "ya.ru" for example.
    
    That means that I can use that dns server exclusively.
    And in tailscale admin page, I can set all machines in vpn to use it as
    dns, overriding their local settings.
    Even phone will be able to resolve my .nope names

--- machines/oracle/morpheus/configuration.nix
+++ machines/oracle/morpheus/configuration.nix
@@ -12,13 +12,18 @@
     zones = {
       "nope" = {
         file = ./from-linux-guide.zone;
-        # file = ./dns-attempt.zone;
         master = true;
         slaves = [
           "127.0.0.1"
         ];
       };
     };
+    cacheNetworks = [ "100.0.0.0/8" "127.0.0.1" ];
+    # this is needed to allow recursive lookup
+    # not sure if there's any way to set this up for ip6 yggdrasil addresses without making them public
+    # well, this could be import of a nix attrset from gitencrypted dir
+    # and if I do "override dns" in tailscale config - my machines use only that dns server, even the phone
+    # NOTE : but then my attempts to set "networking.nameservers" is not quite useful
   };
   networking.firewall.allowedUDPPorts = [ 53 ];
 

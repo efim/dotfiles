814e989ae6c70dd13956cb649c7376d8abdf8f23
efim <efim.nefedov@nordigy.ru>
2022-03-13 13:50:25 +0300

    doom emacs roam v2 config from upstream

--- profiles/my-emacs/doom/init.el
+++ profiles/my-emacs/doom/init.el
@@ -141,7 +141,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +journal +capture +habits +present)               ; organize your plain life in plain text
+       (org +journal +capture +habits +present +roam2)               ; organize your plain life in plain text
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more

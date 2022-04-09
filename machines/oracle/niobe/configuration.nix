e12ca369c16a0a2472c4301a6c23942c2e206b73
efim <efim.nefedov@nordigy.ru>
2022-04-09 16:46:56 +0300

    nix flock setting up searx on niobe
    trying to use NixOS container, would be possible to have stable nixpgks,
    update separately from others, maybe cool?
    
    Initial story is that I've updated franzk, it updated state of
    "nextcloud", but something else broke, system tried to roll back, but
    nextcloud didn't allow it, so state was kind of stuck.
    
    My guess is that with containers, I could potentially be able to
    rollback / update system in a more nuanced way.
    
    But ideally, yeah, use smaller programs, that don't care that much about downgrades

--- machines/oracle/niobe/configuration.nix
+++ machines/oracle/niobe/configuration.nix
@@ -16,11 +16,11 @@
     fd
   ];
 
-  networking.firewall.allowedTCPPorts = [ 13338 ];
+  networking.firewall.allowedTCPPorts = [ 13338 80 ];
   networking.firewall.allowedUDPPorts = [ 13338 ];
   # networking.firewall.allowedTCPPortRanges = [ { from = 51000; to = 51999; } ];
 
-  sops.secrets."yggdrasil/niobe" = {};
+  sops.secrets."yggdrasil/niobe" = { };
   services.yggdrasil = {
     enable = true;
     configFile = config.sops.secrets."yggdrasil/niobe".path;
@@ -40,5 +40,46 @@
 
   services.boinc.enable = true;
   services.boinc.package = inputs.nixpkgs-stable.legacyPackages.x86_64-linux.boinc;
+
+
+  # security.acme.acceptTerms = true;
+  # security.acme.email = "efim.wool@gmail.com";
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    # recommendedTlsSettings = true;
+    virtualHosts = {
+      "searx.nope" = {
+        # enableACME = true;
+        # forceSSL = true;
+        locations."/".proxyPass = "http://192.168.100.11:8080";
+      };
+    };
+  };
+
+  # https://blog.beardhatcode.be/2020/12/Declarative-Nixos-Containers.html
+  networking.nat.enable = true;
+  networking.nat.internalInterfaces = [ "ve-wasabi" ];
+  networking.nat.externalInterface = "ens3"; # ??? ip addr , but do I control this name?
+
+  containers.wasabi = {
+    # ephemeral = true;
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = "192.168.100.2";
+    localAddress = "192.168.100.11";
+
+    config = { config, pkgs, ... }: {
+      services.searx.enable = true;
+      services.searx.settings = {
+        server.port = 8080;
+        server.bind_address = "0.0.0.0";
+        server.secret_key = "hello";
+      };
+
+      networking.firewall.allowedTCPPorts = [ 8080 ];
+      networking.nameservers = [ "1.1.1.1" ];
+    };
+  };
 
 }

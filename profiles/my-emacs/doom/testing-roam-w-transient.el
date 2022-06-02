31967d0dd71fd38a5f234d7a47304776fb7c11f3
efim <efim.nefedov@nordigy.ru>
2022-06-02 02:34:39 +0000

    doom roam transient adding functinal prettiness

--- profiles/my-emacs/doom/testing-roam-w-transient.el
+++ profiles/my-emacs/doom/testing-roam-w-transient.el
@@ -37,18 +37,10 @@
        ))
 
 ;; A plist that contains the various org-roam subject.  Each subject
-;; has a plist of :templates, :title, :name, :path-to-todo, :prefix,
-;; and :group.
+;; has a plist of :templates, :path-to-todo, :prefix
 ;;
 ;; The :templates defines the ;; named templates available for this subject.  See
 ;; `ef/org-roam-capture-templates-plist' for list of valid templates.
-;;
-;; The :name is the string version of the subject, suitable for
-;; creating function names.
-;;
-;; The :title is the human readable "title-case" form of the subject.
-;;
-;; The :group is for `pretty-hydra-define+'
 ;;
 ;; The :prefix helps with menu key build for `pretty-hydra-define+'
 ;;
@@ -62,25 +54,16 @@
              :templates (-non-nil (seq-map-indexed (lambda (template index)
                      (when (cl-evenp index) template))
                    ef/org-roam-capture-templates-plist))
-             :name "all"
-             :title "All"
-             :group "All"
              :prefix "a"
              :filter-fn (lambda (node) t)
              :path-to-todo "~/org/gtd/gtd.org")
        :work (list
                        :templates (list :work)
-                       :name "work"
-                       :title "Work"
-                       :group "Projects"
                        :prefix "w"
                        :filter-fn (lambda (node) (-contains-p (org-roam-node-tags node) "work"))
                        :path-to-todo "~/org/Work/gtd/dins-gtd.org")
        :personal (list
                   :templates (list :personal :general)
-                  :name "personal"
-                  :title "Personal"
-                  :group "Life"
                   :prefix "p"
                   :filter-fn (lambda (node) (-contains-p (org-roam-node-tags node) "personal"))
                   :path-to-todo "~/org/gtd/gtd.org")
@@ -93,23 +76,25 @@
   "Return context set up by 'ef/roam-context'."
   (plist-get ef/org-roam-capture-subjects-plist ef/roam-context))
 
+;; ef/roam-context-from-transient-arg-value - to return key
+;; so that in wrapped - not context, but key
+;; and get templates by key
+;; and do (or key ef/roam-context)
+;; and query the context-plist in place
+;; and ef/saved-context
+
 (cl-defun ef/roam-templates-for-context (context
                                               &key
                                               (template-definitions-plist ef/org-roam-capture-templates-plist))
   "Return a list of `org-roam' templates for the given SUBJECT.
 Use the given (or default) SUBJECTS-PLIST to fetch from the
 given (or default) TEMPLATE-DEFINITIONS-PLIST."
-  (let ((templates (plist-get context :templates)))
-    (-map (lambda (template) (plist-get template-definitions-plist template))
-          templates)))
-
-;; setq ef/org-roam-capture-templates-plist ; these are templates
-;; setq ef/org-roam-capture-subjects-plist ; these are all subjects
+  (-let [(&plist :templates) context]
+    (-map (-partial #'plist-get template-definitions-plist) templates)))
 
 (defun ef/roam-subjects-keys ()
   "All keys for configured subjects."
   (doom-plist-keys ef/org-roam-capture-subjects-plist))
-
 
 (defun ef/roam-subject-clean-key (key)
   "Get roam context key as object from KEY."
@@ -119,11 +104,6 @@ given (or default) TEMPLATE-DEFINITIONS-PLIST."
     ;; default
     (x (error! "Can't treat %s as key" x))))
 
-(defun ef/roam-subject-by-key (key)
-  "Not sure is this is needed, just `plist-get' from list of contextx by KEY."
-  (plist-get ef/org-roam-capture-subjects-plist key))
-
-
 ;;; building the transient
 
 ;; building menu with infixes for each context
@@ -131,16 +111,12 @@ given (or default) TEMPLATE-DEFINITIONS-PLIST."
   (let ((char (plist-get context :prefix)))
     `(,char "" ,(pp-to-string key))))
 
-;; (ef/roam-conext-to-infix (plist-get ef/org-roam-capture-subjects-plist :work))
-
 (setq ef/roam-infix-contexts-list (--map
   (ef/roam-conext-to-infix (plist-get ef/org-roam-capture-subjects-plist it) it)
   (ef/roam-subjects-keys)))
 
 ;; "incompatible contexts"
-(setq ef/roam-incompatible-contexts-list (--map
-  (pp-to-string it)
-  (ef/roam-subjects-keys)))
+(setq ef/roam-incompatible-contexts-list (-map #'pp-to-string (ef/roam-subjects-keys)))
 
 (setq ef/roam-contexts-row-group (seq--into-vector (append `(:class transient-row "Contexts") ef/roam-infix-contexts-list)))
 ;; suffix commands that take selected context for call
@@ -153,32 +129,25 @@ given (or default) TEMPLATE-DEFINITIONS-PLIST."
   "Return the context corresponding to 'transient-args' of main transient.
 If no switch is set return nil."
   (interactive)
-  (let ((args (transient-args
-               'ef/roam-things-transient)))
-    (cl-loop for arg in args
-     return
-     (ef/roam-subject-by-key
-      (ef/roam-subject-clean-key arg)))))
+  (-let [(first-arg) (transient-args 'ef/roam-things-transient)]
+    (plist-get ef/org-roam-capture-subjects-plist (ef/roam-subject-clean-key first-arg))))
 
-;;; need separate wrappers because function differ in what arguments that accept
 ;; org-roam-node-find
 (cl-defun ef/roam-node-find-wrapped-with-context (&optional other-window initial-input &key (input-context nil))
-"Call ROAM-FUNCTION with filter-fn and templates from CONTEXT.
+  "Call ROAM-FUNCTION with filter-fn and templates from CONTEXT.
 OTHER-WINDOW and INITIAL-INPUT passed as is."
   (interactive)
-  (let* ((context (if input-context input-context (ef/roam-saved-context)))
-         (subject-name (plist-get context :name))
-         (filter-fn (plist-get context :filter-fn))
-         (templates (ef/roam-templates-for-context context)))
-    (org-roam-node-find other-window initial-input filter-fn :templates templates)))
+    (let* ((context (or input-context (ef/roam-saved-context)))
+           (filter-fn (plist-get context :filter-fn))
+           (templates (ef/roam-templates-for-context context)))
+      (org-roam-node-find other-window initial-input filter-fn :templates templates)))
 
 ;; org-roam-node-insert
 (cl-defun ef/roam-node-insert-wrapped-with-context (&key (input-context nil))
 "Call ROAM-FUNCTION with filter-fn and templates from CONTEXT.
 OTHER-WINDOW and INITIAL-INPUT passed as is."
   (interactive)
-  (let* ((context (if input-context input-context (ef/roam-saved-context)))
-         (subject-name (plist-get context :name))
+  (let* ((context (or input-context (ef/roam-saved-context)))
          (filter-fn (plist-get context :filter-fn))
          (templates (ef/roam-templates-for-context context)))
     (org-roam-node-insert filter-fn :templates templates)))
@@ -188,8 +157,7 @@ OTHER-WINDOW and INITIAL-INPUT passed as is."
 "Call ROAM-FUNCTION with filter-fn and templates from CONTEXT.
 OTHER-WINDOW and INITIAL-INPUT passed as is."
   (interactive)
-  (let* ((context (if input-context input-context (ef/roam-saved-context)))
-         (subject-name (plist-get context :name))
+  (let* ((context (or input-context (ef/roam-saved-context)))
          (filter-fn (plist-get context :filter-fn))
          (templates (ef/roam-templates-for-context context)))
     (org-roam-capture goto keys :templates templates :filter-fn filter-fn)))
@@ -200,12 +168,10 @@ OTHER-WINDOW and INITIAL-INPUT passed as is."
   (interactive)
   (ef/roam-node-find-wrapped-with-context nil "" :input-context (ef/roam-context-from-transient-arg-value)))
 
-
 (transient-define-suffix ef/roam-contexed-node-insert ()
   "Wrapping `roam-node-insert` with context"
   (interactive)
   (ef/roam-node-insert-wrapped-with-context :input-context (ef/roam-context-from-transient-arg-value)))
-
 
 (transient-define-suffix ef/roam-contexed-capture ()
   "Wrapping `roam-capture` with context"
@@ -216,20 +182,20 @@ OTHER-WINDOW and INITIAL-INPUT passed as is."
 (defun ef/roam-save-context-from-transient-args ()
   "Setting global org-roam context from transient state."
   (interactive)
-  (let* ((args (transient-args
-                'ef/roam-things-transient))
-         (key (ef/roam-subject-clean-key
-               (car args))))
-    (if (not key) (message (format "No context selected, keeping state %s" ef/roam-context))
-      (if (eq key ef/roam-context) (message (format "Same context selected, keeping state %s" ef/roam-context))
-        (if (yes-or-no-p (format "Do you want to set global roam context to %s?" key))
-            (progn
-              (set-variable 'ef/roam-context key)
-              (message (format "New saved roam context: %s" ef/roam-context)))
-          (message (format "Cancelled. Keeping state %s" ef/roam-context)))))))
+  (-let [(first-key) (transient-args 'ef/roam-things-transient)]
+    (pcase (ef/roam-subject-clean-key first-key)
+      ((pred not) (message (format "No context selected, keeping state %s" ef/roam-context)))
+      ((pred (funcall (-partial #'equal ef/roam-context))) (message (format "Same context selected, keeping state %s" ef/roam-context)))
+      ;; default
+      (key (if (yes-or-no-p (format "Do you want to set global roam context to %s?" key))
+             (progn
+               (set-variable 'ef/roam-context key)
+               (message (format "New saved roam context: %s" ef/roam-context)))
+           (message (format "Cancelled. Keeping state %s" ef/roam-context)))))))
 
 (setq ef/roam-actions-transient-suffixes `[
    :description (lambda () (format "Current default context: %s" ef/roam-context))
+   :class transient-row
    "Actions"
    ("?" "find" ef/roam-contexed-node-find)
    ("i" "insert" ef/roam-contexed-node-insert)
@@ -245,8 +211,8 @@ OTHER-WINDOW and INITIAL-INPUT passed as is."
          [
           :class transient-subgroups
           ef/roam-contexts-row-group
-          ]
          ,ef/roam-actions-transient-suffixes
+          ]
          ))
 
 ;; for testing purposes

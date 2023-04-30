7d66fe469f7cd66e61662414f5cb66cefb2da8b2
efim <efim.nefedov@nordigy.ru>
2023-04-30 18:09:17 +0400

    franzk deployment of my alpha apps

--- machines/franzk/configuration.nix
+++ machines/franzk/configuration.nix
@@ -1,11 +1,7 @@
-{ inputs, config, pkgs, ... }:
-{
+{ inputs, config, pkgs, ... }: {
   networking.hostName = "franzk";
 
-  imports = with inputs.self.myRoles; [
-    ./vpsadminos.nix
-    base-server
-  ];
+  imports = with inputs.self.myRoles; [ ./vpsadminos.nix base-server ];
 
   services.syncthing.enable = true;
   # nginx reverse proxy
@@ -13,11 +9,9 @@
   # yay, even without personal dns, I can have one domain per host with tailscala MagicDNS
   # not perfect, but ok
   services.nginx.virtualHosts."franzk.efim.github.beta.tailscale.net" = {
-    locations."/" = {
-        proxyPass = "http://127.0.0.1:8384";
-    };
+    locations."/" = { proxyPass = "http://127.0.0.1:8384"; };
   };
-  networking.firewall.allowedTCPPorts = [ 80 8080 ];
+  networking.firewall.allowedTCPPorts = [ 80 ];
 
   age.secrets.secret1.file = ../../secrets/franzk-server-secret.age;
 
@@ -53,24 +47,44 @@
   services.radicale = {
     enable = true;
     settings = {
-      server = {
-        hosts = [ "0.0.0.0:5232" "[::]:5232" ];
-      };
+      server = { hosts = [ "0.0.0.0:5232" "[::]:5232" ]; };
       auth = {
         type = "htpasswd";
         htpasswd_filename = config.age.secrets.radicale-users.path;
         htpasswd_encryption = "plain";
       };
-      storage = {
-        filesystem_folder = "/var/lib/radicale/collections";
-      };
+      storage = { filesystem_folder = "/var/lib/radicale/collections"; };
     };
   };
   services.nginx.virtualHosts."radicale.sunshine.industries" = {
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:5232";
-    };
+    locations."/" = { proxyPass = "http://127.0.0.1:5232"; };
   };
+
+  services.nginx.virtualHosts."death-calendar.sunshine.industries" = {
+    root = "/var/www/life-expectancy-cal";
+  };
+
+  # # most ugly MVP deploy yet:
+  services.nginx.virtualHosts."planning-poker.sunshine.industries" = {
+    root = "/var/www/planning-poker-grargh"; # copied manually
+  };
+
+  services.nginx.virtualHosts."planning-poker.sunshine.industries".locations."/api" =
+    {
+      proxyPass = "http://127.0.0.1:8080"; # started manually
+      extraConfig = ''
+        rewrite ^/api/(.*)$ /$1 break;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+
+        # Add the following lines for WebSocket support
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+      '';
+    };
 
   services.openssh.enable = true;
   services.openssh.permitRootLogin = "yes";
